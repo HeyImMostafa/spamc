04bdcb49d038d64d628579ec5d9de5da02013e92
HeyImMostafa <44906113+HeyImMostafa@users.noreply.github.com>
2018-11-14 20:38:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', async() => {
 var server = "508279808644612096"; // ايدي السررفر
 var channel = "510097774747058176";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
     },305);
 })
 
